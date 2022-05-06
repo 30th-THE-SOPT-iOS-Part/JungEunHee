c550b34ccbb7571a4ad3fe10c884f1e24165a467
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-07 01:15:41 +0900

    [Del] 접근 지정자 삭제 (#7)

--- Assignment/Instagram_Clone/Instagram_Clone/Source/Common/Consts/Image.swift
+++ Assignment/Instagram_Clone/Instagram_Clone/Source/Common/Consts/Image.swift
@@ -8,26 +8,26 @@
 import UIKit.UIImage
 
 extension Const {
-    public enum Image {
+    enum Image {
         // navagationBarIcon
-        public static let icnBack24 = UIImage(named: "icn_back_24")
-        public static let icnInstagramBlackLogo = UIImage(named: "Instagram Black Logo")
+        static let icnBack24 = UIImage(named: "icn_back_24")
+        static let icnInstagramBlackLogo = UIImage(named: "Instagram Black Logo")
         
         // textFieldIcon
-        public static let icnPasswordHiddenEye = UIImage(named: "icn_password_hidden")
-        public static let icnPasswordShownEye = UIImage(named: "icn_password_shown")
+        static let icnPasswordHiddenEye = UIImage(named: "icn_password_hidden")
+        static let icnPasswordShownEye = UIImage(named: "icn_password_shown")
         
         // tapBarIcon
-        public static let icnHome = UIImage(named: "icn_home")
-        public static let icnHomeSelected = UIImage(named: "icn_home_selected")
-        public static let icnSearch = UIImage(named: "icn_search")
-        public static let icnSearchSelected = UIImage(named: "icn_search_selected")
-        public static let icnReels = UIImage(named: "icn_reels")
-        public static let icnReelsSelected = UIImage(named: "icn_reels_selected")
-        public static let icnShop = UIImage(named: "icn_shop")
-        public static let icnShopSelected = UIImage(named: "icn_shop_selected")
-        public static let icnProfile = UIImage(named: "icn_profile")
-        public static let icnProfileSelected = UIImage(named: "icn_profile_selected")
+        static let icnHome = UIImage(named: "icn_home")
+        static let icnHomeSelected = UIImage(named: "icn_home_selected")
+        static let icnSearch = UIImage(named: "icn_search")
+        static let icnSearchSelected = UIImage(named: "icn_search_selected")
+        static let icnReels = UIImage(named: "icn_reels")
+        static let icnReelsSelected = UIImage(named: "icn_reels_selected")
+        static let icnShop = UIImage(named: "icn_shop")
+        static let icnShopSelected = UIImage(named: "icn_shop_selected")
+        static let icnProfile = UIImage(named: "icn_profile")
+        static let icnProfileSelected = UIImage(named: "icn_profile_selected")
     }
     
 }
