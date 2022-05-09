9a53ecf587aa2098ad7cbb156ba55cbf789d5b41
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-09 16:41:23 +0900

    [Style] HeaderView UI 구성 (#7)

---
+++ Assignment/Instagram_Clone/Instagram_Clone/Source/Presentation/Main/Views/Cells/HomeHeaderView.swift
@@ -0,0 +1,16 @@
+//
+//  HomeHeaderView.swift
+//  Instagram_Clone
+//
+//  Created by 정은희 on 2022/05/09.
+//
+
+import UIKit
+
+final class HomeHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
