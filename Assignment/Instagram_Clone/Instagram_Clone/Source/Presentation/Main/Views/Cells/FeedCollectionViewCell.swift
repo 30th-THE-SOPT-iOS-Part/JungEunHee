fd6d1a0f58f4e1af7cfa7ef44b6150d41bb07b5e
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-09 17:05:54 +0900

    [Style] 스토리, 피드 셀 UI 구성 (#7)

---
+++ Assignment/Instagram_Clone/Instagram_Clone/Source/Presentation/Main/Views/Cells/FeedCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCollectionViewCell.swift
+//  Instagram_Clone
+//
+//  Created by 정은희 on 2022/05/09.
+//
+
+import UIKit
+
+final class FeedCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
---
+++ Assignment/Instagram_Clone/Instagram_Clone/Source/Presentation/Main/Views/Cells/StoryCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  StoryCollectionViewCell.swift
+//  Instagram_Clone
+//
+//  Created by 정은희 on 2022/05/09.
+//
+
+import UIKit
+
+final class StoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
